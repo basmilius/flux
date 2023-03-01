c08f8c32cd73ed7a69bc9f322cfe1ff5d92225b8
Bas Milius <basmilius@hotmail.com>
2023-03-01 09:30:59 +0100

    feat(pane): adds pane tags.

--- packages/flux/src/components/FluxPane.vue
+++ packages/flux/src/components/FluxPane.vue
@@ -7,6 +7,12 @@
             class="flux-pane-overlay">
             <flux-spinner/>
         </div>
+
+        <div
+            v-if="tag"
+            class="flux-pane-tag">
+            {{ tag }}
+        </div>
     </flux-surface>
 </template>
 
@@ -17,6 +23,7 @@
 
     export interface Props {
         readonly isLoading?: boolean;
+        readonly tag?: string;
     }
 
     defineProps<Props>();
@@ -37,6 +44,22 @@
             background: var(--surface-overlay);
             border-radius: inherit;
             z-index: 100;
+        }
+
+        &-tag {
+            position: absolute;
+            top: 0;
+            left: 21px;
+            padding: 6px 9px;
+            background: var(--gray-10);
+            border-radius: calc(var(--radius) / 2);
+            color: var(--gray-0);
+            font-size: 11px;
+            font-weight: 700;
+            letter-spacing: 1px;
+            line-height: 1;
+            text-transform: uppercase;
+            translate: 0 -50%;
         }
     }
 </style>
