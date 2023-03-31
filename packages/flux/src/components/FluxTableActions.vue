d89e6663399a52085124492dae22a5f6d69b74f6
Bas Milius <basmilius@hotmail.com>
2023-03-31 09:40:48 +0200

    fix(table): actions scroll in table not in a pane.

--- packages/flux/src/components/FluxTableActions.vue
+++ packages/flux/src/components/FluxTableActions.vue
@@ -12,8 +12,12 @@
 <style lang="scss">
     .flux-table-actions {
         display: flex;
-        margin: -3px;
+        margin: -3px 0 -3px -3px;
         flex-flow: row nowrap;
         gap: 1px;
+    }
+
+    .flux-pane > .flux-table .flux-table-actions {
+        margin-right: -3px;
     }
 </style>
