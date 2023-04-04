4bf5d5403eba95b776a2736b788c7b52f5cc98b6
Bas Milius <basmilius@hotmail.com>
2023-04-04 21:06:53 +0200

    feat(comment): introduce comment component.

--- packages/docs/src/pages/Playground.vue
+++ packages/docs/src/pages/Playground.vue
@@ -7,16 +7,9 @@
         <section>
             <preview>
                 <flux-pane>
-                    <flux-pane-header title="Pane with Notice"/>
-                    <flux-notice-stack>
-                        <flux-notice
-                            icon="circle-exclamation"
-                            is-small
-                            message="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque dolor, et minima nemo neque odio odit recusandae saepe suscipit. Beatae commodi dolorem fugit nemo omnis quam suscipit tenetur voluptate?"
-                            variant="info"/>
-                    </flux-notice-stack>
-                    <flux-pane-body>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae delectus, deserunt, ducimus ea enim fuga illo laboriosam magni maxime omnis provident quae quaerat qui repellat repudiandae rerum similique suscipit velit!
+                    <flux-pane-body :gap="30">
+                        <flux-comment is-flipped/>
+                        <flux-comment is-received/>
                     </flux-pane-body>
                 </flux-pane>
             </preview>
@@ -31,6 +24,6 @@
 <script
     lang="ts"
     setup>
-    import { FluxNotice, FluxNoticeStack, FluxPane, FluxPaneBody, FluxPaneHeader, FluxStack } from '@fancee/flux';
+    import { FluxComment, FluxPane, FluxPaneBody, FluxStack } from '@fancee/flux';
     import { PageTitle, Preview } from '@/components';
 </script>
---
+++ packages/flux/src/components/FluxComment.vue
@@ -0,0 +1,112 @@
+<template>
+    <div
+        class="flux-comment"
+        :class="{
+            'is-flipped': isFlipped,
+            'is-received': isReceived
+        }">
+        <flux-avatar
+            class="flux-comment-avatar"
+            fallback-initials="BM"
+            :size="42"/>
+
+        <div class="flux-comment-body">
+            <div class="flux-comment-header">
+                <strong>Bas Milius</strong>
+                <time>13:03</time>
+            </div>
+
+            <div class="flux-comment-message">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic ipsum nihil quia vero? Et eveniet ex hic id impedit ipsa necessitatibus perferendis quibusdam totam ut? Assumenda deleniti dolorum recusandae tenetur?
+            </div>
+        </div>
+    </div>
+</template>
+
+<script
+    lang="ts"
+    setup>
+    import { FluxAvatar } from '.';
+
+    export interface Props {
+        readonly isFlipped?: boolean;
+        readonly isReceived?: boolean;
+    }
+
+    defineProps<Props>();
+</script>
+
+<style lang="scss">
+    .flux-comment {
+        display: flex;
+        gap: 21px;
+
+        &-avatar {
+            flex-shrink: 0;
+        }
+
+        &-body {
+            display: flex;
+            align-items: flex-start;
+            flex-flow: column;
+            gap: 9px;
+        }
+
+        &-header {
+            display: flex;
+            padding: 3px 15px 0;
+            align-items: center;
+            align-self: stretch;
+            gap: 15px;
+            justify-content: space-between;
+
+            strong {
+                color: var(--foreground-prominent);
+            }
+
+            time {
+                color: var(--foreground-secondary);
+                font-size: 14px;
+            }
+        }
+
+        &.is-flipped &-header {
+            padding-right: 0;
+        }
+
+        &:not(.is-flipped) &-header {
+            padding-left: 0;
+        }
+
+        &-message {
+            padding: 12px 15px;
+            background: linear-gradient(to bottom, rgb(var(--gray-0)), rgb(var(--gray-2)));
+            border: 1px solid rgb(var(--gray-4));
+            border-radius: calc(var(--radius) * 1.5);
+            box-shadow: var(--shadow-md);
+        }
+
+        &.is-flipped &-message {
+            border-top-right-radius: calc(var(--radius) * .5);
+        }
+
+        &:not(.is-flipped) &-message {
+            border-top-left-radius: calc(var(--radius) * .5);
+        }
+
+        &.is-received &-message {
+            background: linear-gradient(to bottom, rgb(var(--primary-6)), rgb(var(--primary-8)));
+            border-color: rgb(var(--primary-9));
+            color: rgb(var(--primary-0));
+        }
+
+        &.is-flipped &-avatar {
+            order: 1;
+        }
+
+        &.is-flipped &-body {
+            margin-left: auto;
+            align-items: flex-end;
+        }
+    }
+</style>
