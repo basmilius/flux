a0e997858d2199c337572088e46d01fadf122348
Bas Milius <basmilius@hotmail.com>
2023-04-12 08:44:06 +0200

    docs: tooltip examples.

---
+++ packages/docs/src/code/components/tooltip/basic.vue
@@ -0,0 +1,14 @@
+<template>
+    <flux-tooltip content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur beatae cupiditate deserunt eius, fugiat ipsam maiores perspiciatis praesentium quae quasi quisquam quos, reiciendis rem repellendus, temporibus ut vitae voluptates.">
+        <flux-secondary-button label="Button"/>
+    </flux-tooltip>
+</template>
+
+<script
+    lang="ts"
+    setup>
+    import {
+        FluxSecondaryButton,
+        FluxTooltip
+    } from '@fancee/flux';
+</script>
