33273279c67235763583503d0db488b8bef83f59
Bas Milius <basmilius@hotmail.com>
2023-05-24 13:20:16 +0200

    feat(focal-point): adds focal point within gallery items.

--- packages/flux/src/components/FluxGallery.vue
+++ packages/flux/src/components/FluxGallery.vue
@@ -14,13 +14,22 @@
                 class="flux-gallery"
                 name="flux-gallery"
                 tag="div">
-                <FluxGalleryItem
-                    v-if="items"
-                    v-for="(item, index) of items"
-                    :is-deletable="isEditable"
-                    :key="item"
-                    :url="item"
-                    @delete="$emit('delete', index)"/>
+                <template v-if="items" v-for="(item, index) of items">
+                    <FluxGalleryItem
+                        v-if="typeof item === 'string'"
+                        :is-deletable="isEditable"
+                        :key="item"
+                        :url="item"
+                        @delete="$emit('delete', index)"/>
+
+                    <FluxGalleryItem
+                        v-else
+                        :is-deletable="isEditable"
+                        :key="item.url"
+                        :focal-point="item"
+                        :url="item.url"
+                        @delete="$emit('delete', index)"/>
+                </template>
 
                 <slot/>
 
@@ -49,6 +58,7 @@
     import FluxDropZone from './FluxDropZone.vue';
     import FluxGalleryItem from './FluxGalleryItem.vue';
     import FluxIcon from './FluxIcon.vue';
+    import { FluxFocalPoint } from '@/data';
 
     export interface Emits {
         (e: 'delete', index: number): void;
@@ -58,7 +68,7 @@
 
     export interface Props {
         readonly isEditable?: boolean;
-        readonly items?: string[];
+        readonly items?: (string | (FluxFocalPoint & { readonly url: string; }))[];
         readonly pendingItems?: string[];
     }
 
