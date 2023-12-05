cccded47ec6ba696485ee8f197db3c0946f0d71a
Bas Milius <basmilius@hotmail.com>
2023-12-05 10:52:22 +0100

    chore: more vue 3 optimizations.

--- packages/flux/src/utils/vue.ts
+++ packages/flux/src/utils/vue.ts
@@ -1,4 +1,4 @@
-import { Component, Fragment, h, Ref, unref, UnwrapRef, VNode } from 'vue';
+import { Fragment, Ref, unref, UnwrapRef, VNode } from 'vue';
 import { hyphenateTag } from './dom';
 
 export function assertRefNotNull<T>(ref: Ref<T>): asserts ref is Ref<NonNullable<T>> {
@@ -22,86 +22,18 @@ export function flattenVNodeTree(vnodes: VNode[]): VNode[] {
     return flattened;
 }
 
-export function getNormalizedComponentName(component: any | Vue2Component): string {
+export function getNormalizedComponentName(component: ExtendedVNode): string {
     let name = 'flux-unknown';
 
     if (component.type && component.type.__name) {
         name = component.type.__name;
     }
 
-    if (component.componentOptions && component.componentOptions.tag) {
-        name = component.componentOptions.tag;
-    }
-
     return hyphenateTag(name);
 }
 
 export function getNormalizedComponentProps<T extends object>(component: any): T {
-    if (component.props) {
-        return component.props;
-    }
-
-    if (component.componentOptions && component.componentOptions.propsData) {
-        return component.componentOptions.propsData;
-    }
-
-    return {} as T;
-}
-
-export function isVNode(vnode: unknown): vnode is VNode {
-    return typeof vnode === 'object' && !!vnode && (vnode.hasOwnProperty('__v_isVNode') || vnode.hasOwnProperty('componentOptions'));
-}
-
-export function render<P>(component: Component<P>, spec?: RenderSpec<P>): VNode {
-    if (!spec) {
-        return h(component);
-    }
-
-    const {
-        attrs,
-        children,
-        on,
-        props,
-        slots
-    } = spec;
-
-    if (children && slots) {
-        throw new Error('[Flux] Cannot use children and slots at the same time.');
-    }
-
-    const options: any = {};
-    let content;
-
-    // Vue 2.7
-    if ('staticRenderFns' in component) {
-        options.domProps = attrs;
-        options.on = on;
-        options.props = props;
-        options.scopedSlots = slots;
-        content = children;
-    } else {
-        if (on) {
-            for (let key in on) {
-                options[`on${key.substring(0, 1).toUpperCase()}${key.substring(1)}`] = on[key];
-            }
-        }
-
-        if (attrs) {
-            for (let key in attrs) {
-                options[key] = attrs[key];
-            }
-        }
-
-        if (props) {
-            for (let key in props) {
-                options[key] = props[key];
-            }
-        }
-
-        content = slots || children;
-    }
-
-    return h(component, options, content);
+    return component.props ?? {} as T;
 }
 
 export function unrefObject<T>(obj: RefObject<T>): UnrefObject<T> {
@@ -113,14 +45,6 @@ export function unrefObject<T>(obj: RefObject<T>): UnrefObject<T> {
     ) as UnrefObject<T>;
 }
 
-interface RenderSpec<P> {
-    readonly attrs?: Record<string, unknown>;
-    readonly children?: RawChildren;
-    readonly on?: Record<string, Function>;
-    readonly props?: Partial<P>;
-    readonly slots?: RawSlots;
-}
-
 type RefObject<T = Record<string, Ref<unknown>>> = {
     [K in keyof T]: T[K];
 };
@@ -129,12 +53,8 @@ type UnrefObject<T> = {
     [K in keyof T]: UnwrapRef<T[K]>;
 };
 
-type RawChildren = string | number | boolean | VNode | VNode[] | (() => any);
-type RawSlots = { [name: string]: unknown; };
-
-interface Vue2Component {
-    readonly componentOptions: {
-        readonly propsData?: object;
-        readonly tag?: string;
+interface ExtendedVNode {
+    readonly type: VNode['type'] & {
+        readonly __name?: string;
     };
 }
