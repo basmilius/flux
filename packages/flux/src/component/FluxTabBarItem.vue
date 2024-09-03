8ee24c8edd8c5b7fcb11347b5617b6e941e6cf3d
Bas Milius <basmilius@hotmail.com>
2024-09-03 19:43:01 +0200

    feat(tabs): tab bar items now use the base button component, which allows them to be used as router links. feat(toolbar): changed separator height back to how it was before. chore: bump deps.

--- packages/flux/src/component/FluxTabBarItem.vue
+++ packages/flux/src/component/FluxTabBarItem.vue
@@ -1,43 +1,80 @@
 <template>
-    <button
+    <ButtonComponent
         ref="tabRef"
+        :component-type="type"
         :class="isActive ? styles.tabBarItemActive : styles.tabBarItem"
         type="button"
         role="tab"
+        :aria-disabled="disabled ? true : undefined"
         :aria-selected="isActive"
-        @click="$emit('click', $event)">
+        :disabled="disabled ? true : undefined"
+        :tabindex="disabled ? -1 : tabindex"
+        :href="href"
+        :rel="rel"
+        :target="target"
+        :to="to"
+        @click="onClick"
+        @mouseenter="onMouseEnter"
+        @mouseleave="onMouseLeave">
         <FluxIcon
             v-if="icon"
             :size="16"
             :variant="icon"/>
 
         <span v-if="label">{{ label }}</span>
-    </button>
+    </ButtonComponent>
 </template>
 
 <script
     lang="ts"
     setup>
     import { ref, toRefs, unref, watch } from 'vue';
-    import type { IconNames } from '@/data';
+    import type { FluxRoutingLocation, IconNames } from '@/data';
+    import ButtonComponent from '@/component/primitive/ButtonComponent.vue';
     import FluxIcon from './FluxIcon.vue';
     import styles from '@/css/component/Tab.module.scss';
 
     export type Emits = {
         click: [MouseEvent];
+        mouseenter: [MouseEvent];
+        mouseleave: [MouseEvent];
     };
 
     export type Props = {
+        readonly type?: 'button' | 'link' | 'route';
+        readonly disabled?: boolean;
         readonly icon?: IconNames;
         readonly isActive?: boolean;
         readonly label?: string;
+        readonly tabindex?: string | number;
+        readonly href?: string;
+        readonly rel?: string;
+        readonly target?: string;
+        readonly to?: FluxRoutingLocation;
     };
 
-    defineEmits<Emits>();
+    const emit = defineEmits<Emits>();
     const props = defineProps<Props>();
-    const {isActive} = toRefs(props);
+    const {disabled, isActive} = toRefs(props);
 
     const tabRef = ref<HTMLButtonElement>();
+
+    function onClick(evt: MouseEvent): void {
+        if (unref(disabled)) {
+            evt.preventDefault();
+            evt.stopPropagation();
+        }
+
+        emit('click', evt);
+    }
+
+    function onMouseEnter(evt: MouseEvent): void {
+        emit('mouseenter', evt);
+    }
+
+    function onMouseLeave(evt: MouseEvent): void {
+        emit('mouseleave', evt);
+    }
 
     watch(isActive, isActive => {
         if (!isActive) {
