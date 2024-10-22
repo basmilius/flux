c78e53e537b42c173e5fe5e32750a6bae16ee639
Bas Milius <basmilius@hotmail.com>
2024-10-22 12:43:31 +0200

    chore: update workflow. chore: remove old documentation code.

--- _old/code/components/progressBar/basic.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <FluxProgressBar :value="0.5"/>
-</template>
-
-<script
-    setup
-    lang="ts">
-    import { FluxProgressBar } from '@basmilius/flux';
-</script>
--- _old/code/components/spinner/basic.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <FluxSpinner/>
-</template>
-
-<script
-    lang="ts"
-    setup>
-    import { FluxSpinner } from '@basmilius/flux';
-</script>
