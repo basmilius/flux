34404159a1c72533e24d83ba00984529b86bdea0
Bas Milius <basmilius@hotmail.com>
2024-12-06 09:36:45 +0100

    chore: bump deps. chore: simplified vite configuration files.

--- packages/dashboard/vite.config.ts
+++ packages/dashboard/vite.config.ts
@@ -1,27 +1,22 @@
-import { createHash } from 'node:crypto';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
-import { patchCssModules } from 'vite-css-modules';
+import preset from '@basmilius/vite-vue-preset';
 import vue from '@vitejs/plugin-vue';
-import dts from 'vite-plugin-dts';
-import className from 'css-class-generator';
 
 export default defineConfig(({command}) => ({
     plugins: [
-        patchCssModules(),
-        vue(),
-        dts({
-            cleanVueFileName: true
-        })
+        preset({
+            isLibrary: true
+        }),
+        vue()
     ],
     build: {
+        assetsDir: '',
         emptyOutDir: command === 'build',
-        cssMinify: 'esbuild',
-        minify: 'esbuild',
-        outDir: resolve(import.meta.dirname, './dist'),
+        outDir: resolve(import.meta.dirname, 'dist'),
         sourcemap: true,
         lib: {
-            entry: resolve(import.meta.dirname, './src/index.ts'),
+            entry: resolve(import.meta.dirname, 'src/index.ts'),
             fileName: 'flux-dashboard',
             name: 'fluxDashboard'
         },
@@ -38,34 +33,7 @@ export default defineConfig(({command}) => ({
             }
         }
     },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                api: 'modern-compiler'
-            }
-        },
-        modules: {
-            generateScopedName(name: string): string {
-                if (name.startsWith('i__const_')) {
-                    name = name.substring(9);
-                    name = name.substring(0, name.length - 2);
-                }
-
-                const hash = createHash('sha1')
-                    .update(name)
-                    .digest('hex')
-                    .substring(0, 5);
-
-                return className(parseInt(hash, 16));
-            }
-        }
-    },
     define: {
         __VUE_OPTIONS_API__: 'false'
-    },
-    resolve: {
-        alias: {
-            '@': resolve(import.meta.dirname, './src')
-        }
     }
 }));
