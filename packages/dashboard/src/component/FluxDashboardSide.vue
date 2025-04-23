16856e2d3907452be3b276e3f2abf9177f590907
Bas Milius <basmilius@hotmail.com>
2025-04-23 20:43:19 +0200

    chore: work on docs. chore: updated css. chore: bump deps.

--- packages/dashboard/src/component/FluxDashboardSide.vue
+++ packages/dashboard/src/component/FluxDashboardSide.vue
@@ -19,7 +19,7 @@
     setup>
     import { FluxSpacer } from '@flux-ui/components';
     import FluxDashboardTopBar from './FluxDashboardTopBar.vue';
-    import $style from '$fluxDashboard/css/component/DashboardTopBar.module.scss';
+    import $style from '$fluxDashboard/css/component/DashboardPane.module.scss';
 
     defineProps<{
         readonly title: string;
