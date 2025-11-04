ade241dfbe90c60ed1e331ef616fa37c02b3e150
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2025-11-04 12:35:25 +0100

    Chore(container): Added documentation

---
+++ docs/code/guide/components/layout/container/preview.vue
@@ -0,0 +1,17 @@
+<template>
+    <Preview>
+        <FluxContainer>
+            <FluxPane>
+                <FluxPaneBody>
+
+                </FluxPaneBody>
+            </FluxPane>
+        </FluxContainer>
+    </Preview>
+</template>
+
+<script
+    setup
+    lang="ts">
+    import { FluxContainer, FluxPane, FluxPaneBody } from '@flux-ui/components';
+</script>
