b1c02d5dd112fa7d48b8436bb7a30ceb3fb36d36
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2025-11-04 14:51:09 +0100

    Chore(spacer): Added documentation for spacer

---
+++ docs/code/guide/components/layout/spacer/vertical.vue
@@ -0,0 +1,25 @@
+<template>
+    <FluxPane>
+        <FluxPaneBody>
+            <FluxButtonStack direction="vertical">
+                <FluxSecondaryButton
+                    label="More info"/>
+
+                <FluxSpacer class="spacer-example"/>
+
+                <FluxSecondaryButton
+                    label="Cancel"/>
+
+                <FluxPrimaryButton
+                    icon-leading="circle-check"
+                    label="Save"/>
+            </FluxButtonStack>
+        </FluxPaneBody>
+    </FluxPane>
+</template>
+
+<script
+    setup
+    lang="ts">
+    import { FluxButtonStack, FluxPane, FluxPaneBody, FluxPrimaryButton, FluxSecondaryButton, FluxSpacer } from '@flux-ui/components';
+</script>
---
+++ docs/guide/components/layout/spacer.md
@@ -0,0 +1,23 @@
+---
+outline: deep
+---
+
+# Spacer
+
+A component that fills remaining space in its parent container, helpful in creating evenly divided button rows in responsive layouts. Spacers are invisible elements and are made visible for demo purposes.
+
+::: render
+render=../../../code/guide/components/layout/spacer/preview.vue
+:::
+
+<FrontmatterDocs/>
+
+## Examples
+
+::: example Horizontal || A horizontal spacer.
+example=../../../code/guide/components/layout/spacer/horizontal.vue
+:::
+
+::: example Vertical || A vertical spacer.
+example=../../../code/guide/components/layout/spacer/vertical.vue
+:::
