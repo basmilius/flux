1888454d1cdc6171f603ed61063439b5073d770a
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2025-11-04 12:26:07 +0100

    Chore(aspect-ratio): Added example for aspect ratio

---
+++ docs/code/guide/components/layout/aspect-ratio/basic.vue
@@ -0,0 +1,13 @@
+<template>
+    <FluxAspectRatio :aspect-ratio="16 / 9">
+        <FluxPane style="align-items: center; display: flex; justify-content: center;">
+            16:9
+        </FluxPane>
+    </FluxAspectRatio>
+</template>
+
+<script
+    setup
+    lang="ts">
+    import { FluxAspectRatio, FluxPane } from '@flux-ui/components';
+</script>
