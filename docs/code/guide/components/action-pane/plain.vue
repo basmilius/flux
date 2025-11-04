cc20f3e0ff8d0139afa16fcc6e929053ec179e0b
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2025-11-04 12:18:13 +0100

    Chore(action-pane): Updated documentation and added examples

---
+++ docs/code/guide/components/action-pane/plain.vue
@@ -0,0 +1,13 @@
+<template>
+    <FluxActionPane>
+        <p>
+            <strong>Premium</strong> &mdash; Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias at beatae exercitationem explicabo fugiat laborum libero magnam nulla odio, officia quaerat similique, totam ullam voluptate! Ad eaque iste voluptatibus.
+        </p>
+    </FluxActionPane>
+</template>
+
+<script
+    setup
+    lang="ts">
+    import { FluxActionPane } from '@flux-ui/components';
+</script>
