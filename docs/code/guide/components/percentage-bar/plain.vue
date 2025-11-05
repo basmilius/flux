e7e000e7fc5e82ef095d9824d236c782a2038111
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2025-11-05 21:29:53 +0100

    Chore(percentage-bar): Added documentation for percentage bar

---
+++ docs/code/guide/components/percentage-bar/plain.vue
@@ -0,0 +1,27 @@
+<template>
+    <FluxPane>
+        <FluxPaneHeader
+            icon="store"
+            title="Expenses at each store"/>
+        <FluxPaneBody>
+            <FluxPercentageBar
+                :items="items"/>
+        </FluxPaneBody>
+    </FluxPane>
+</template>
+
+<script
+    setup
+    lang="ts">
+    import { FluxPane, FluxPaneBody, FluxPaneHeader, FluxPercentageBar } from '@flux-ui/components';
+    import type { FluxPercentageBarItemObject } from '@flux-ui/types';
+    import { ref } from 'vue';
+
+    const items = ref<FluxPercentageBarItemObject[]>([
+        { label: 'Aldi', color: '#00529B', icon: "store", value: 0.25 },
+        { label: 'Albert Heijn', color: '#00A1E4', icon: "store", value: 0.1 },
+        { label: 'Jumbo', color: '#FFD700', icon: "store", value: 0.5 },
+        { label: 'Plus', color: '#6BA539', icon: "store", value: 0.1 },
+        { label: 'Other', color: '#CBD5E1', icon: "store", value: 0.05 }
+    ]);
+</script>
