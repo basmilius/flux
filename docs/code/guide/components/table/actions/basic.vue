ce39a2f3b121d8e93232fb82b86deb549c51c00f
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2025-11-08 02:13:11 +0100

    Chore(table): Added documentation for table

---
+++ docs/code/guide/components/table/actions/basic.vue
@@ -0,0 +1,35 @@
+<template>
+    <FluxPane>
+        <FluxTable>
+            <template #header>
+                <FluxTableHeader>
+                    Header
+                </FluxTableHeader>
+                <FluxTableHeader is-shrinking>
+                    Actions
+                </FluxTableHeader>
+            </template>
+
+            <FluxTableRow>
+                <FluxTableCell>
+                    Row
+                </FluxTableCell>
+
+                <FluxTableCell>
+                    <FluxTableActions>
+                        <FluxAction
+                            icon="pen"/>
+                        <FluxAction
+                            icon="ellipsis-h"/>
+                    </FluxTableActions>
+                </FluxTableCell>
+            </FluxTableRow>
+        </FluxTable>
+    </FluxPane>
+</template>
+
+<script
+    setup
+    lang="ts">
+    import { FluxAction, FluxPane, FluxTable, FluxTableActions, FluxTableCell, FluxTableHeader, FluxTableRow } from '@flux-ui/components';
+</script>
---
+++ docs/code/guide/components/table/header/shrinking.vue
@@ -0,0 +1,30 @@
+<template>
+    <FluxPane>
+        <FluxTable>
+            <template #header>
+                <FluxTableHeader
+                    v-for="header in 3">
+                    Header {{ header }}
+                </FluxTableHeader>
+                <FluxTableHeader is-shrinking/>
+            </template>
+
+            <FluxTableRow>
+                <FluxTableCell
+                    v-for="cell in 3">
+                    Cell 1&times;{{ cell }}
+                </FluxTableCell>
+
+                <FluxTableCell>
+                    ...
+                </FluxTableCell>
+            </FluxTableRow>
+        </FluxTable>
+    </FluxPane>
+</template>
+
+<script
+    setup
+    lang="ts">
+    import { FluxPane, FluxTable, FluxTableCell, FluxTableHeader, FluxTableRow } from '@flux-ui/components';
+</script>
