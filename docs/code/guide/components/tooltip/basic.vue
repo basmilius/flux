ddf4a85f4715c912e53a8087c54a35f97d921f31
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2025-11-08 03:02:38 +0100

    Chore(tooltip): Added documentation for tooltip

---
+++ docs/code/guide/components/tooltip/basic.vue
@@ -0,0 +1,14 @@
+<template>
+    <FluxTooltip
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur beatae cupiditate deserunt eius, fugiat ipsam maiores perspiciatis praesentium quae quasi quisquam quos, reiciendis rem repellendus, temporibus ut vitae voluptates.">
+        <FluxSecondaryButton
+            icon-leading="copy"
+            label="Hover me"/>
+    </FluxTooltip>
+</template>
+
+<script
+    setup
+    lang="ts">
+    import { FluxSecondaryButton, FluxTooltip } from '@flux-ui/components';
+</script>
