166d70c1d63f4453e6f38dd55111aa4f3635c7dc
Bas Milius <basmilius@hotmail.com>
2026-03-20 12:51:20 +0100

    fix: use semantic list markup with stable key in KeyboardShortcuts

--- docs/.vitepress/theme/KeyboardShortcuts.vue
+++ docs/.vitepress/theme/KeyboardShortcuts.vue
@@ -1,11 +1,13 @@
 <template>
     <h2 id="keyboard-shortcuts">Keyboard shortcuts</h2>
 
-    <p v-for="({key, action}) of shortcuts">
-        <code><strong>{{ key }}</strong></code>
-        <br>
-        {{ action }}
-    </p>
+    <ul>
+        <li v-for="({key, action}) of shortcuts" :key="`${key}-${action}`">
+            <code><strong>{{ key }}</strong></code>
+            <br>
+            {{ action }}
+        </li>
+    </ul>
 </template>
 
 <script
