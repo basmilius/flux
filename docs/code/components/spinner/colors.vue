f1976d8f1b327bcf24c4511ef655d594d150d855
Bas Milius <basmilius@hotmail.com>
2026-06-29 13:23:20 +0200

    feat(spinner): add color and label props, improve accessibility documentation feat(quantity-selector): fix input sizing with onMounted, improve accessibility with aria-label feat(info): add color prop, improve usage documentation and examples feat(menu): add label prop for improved accessibility feat(form): improve step snapping description and add aria-label for inputs

---
+++ docs/code/components/spinner/colors.vue
@@ -0,0 +1,35 @@
+<template>
+    <FluxFlex
+        direction="horizontal"
+        :gap="21">
+        <FluxSpinner
+            color="primary"
+            :size="36"/>
+
+        <FluxSpinner
+            color="danger"
+            :size="36"/>
+
+        <FluxSpinner
+            color="info"
+            :size="36"/>
+
+        <FluxSpinner
+            color="success"
+            :size="36"/>
+
+        <FluxSpinner
+            color="warning"
+            :size="36"/>
+
+        <FluxSpinner
+            color="gray"
+            :size="36"/>
+    </FluxFlex>
+</template>
+
+<script
+    setup
+    lang="ts">
+    import { FluxFlex, FluxSpinner } from '@flux-ui/components';
+</script>
